6a3c2834e5e4aca0da5013e8a9b426e0ce903776
moook-git <moook.email@gmail.com>
2024-08-05 16:59:55 +0300

    Add new pages

--- scripts/main.js
+++ scripts/main.js
@@ -25,3 +25,47 @@ new Swiper('section.continuity div.swiper', {
         clickable: true
     }
 })
+
+new Swiper('section.gallery div.swiper', {
+    slidesPerView: 1,
+    navigation: {
+        prevEl: document.querySelector('section.gallery div.swiper-navigation > *:first-child'),
+        nextEl: document.querySelector('section.gallery div.swiper-navigation > *:last-child')
+    },
+    pagination: {
+        el: document.querySelector('section.gallery div.swiper-pagination'),
+        clickable: true
+    },
+    on: {
+        init: handleSlideChange,
+        transitionEnd: handleSlideChange
+    }
+})
+
+
+let tabCaptionsSlider = new Swiper('div.swiper.tabs_captions', {
+    slidesPerView: 'auto',
+    spaceBetween: 40,
+    slideToClickedSlide: true
+})
+
+
+let tabContensSlider = new Swiper('div.swiper.tabs_contents', {
+    autoHeight: true,
+    thumbs: {
+        swiper: tabCaptionsSlider
+    }
+})
+
+tabContensSlider.on('slideChange', () => { tabCaptionsSlider.slideTo(tabContensSlider.activeIndex) })
+tabCaptionsSlider.on('slideChange', () => { tabContensSlider.slideTo(tabCaptionsSlider.activeIndex) })
+
+function handleSlideChange (swiper) {
+    var currentSlide = this.realIndex + 1
+    var totalSlides = this.slides.length
+    var counterCurrent = document.querySelector('section.gallery div.counter span.current')
+    var counterTotal = document.querySelector('section.gallery div.counter span.total')
+
+    counterCurrent.innerHTML = currentSlide.toString().padStart(2, '0')
+    counterTotal.innerHTML = '/ ' + totalSlides.toString().padStart(2, '0')
+}
